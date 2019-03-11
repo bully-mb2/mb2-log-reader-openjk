7ee81f923f749341e8636e59ed12298fcb84293c
Caelum <me@caelish.com>
2019-03-11 18:14:46 +0000

    Actual code for sv_antilaghack -> same author as DST but allows a lag exploit through patched DLL. Also: echo "www.jka.io anti-cheat" message instead of "was kicked" message because by the time this rolls out, people will probably know what's going on and there'll be no point in hiding it anymore

--- codemp/server/sv_client.cpp
+++ codemp/server/sv_client.cpp
@@ -1216,8 +1216,7 @@ void SV_UserinfoChanged( client_t *cl ) {
 	if (sv_antiDST->integer) {
 		val = Info_ValueForKey(cl->userinfo, "model");
 		if (!Q_stricmpn(val, "darksidetools", 13) && cl->netchan.remoteAddress.type != NA_LOOPBACK) {
-			//SV_DropClient(cl, "was dropped by TnG!");
-			SV_DropClient(cl, SV_GetStringEdString("MP_SVGAME", "WAS_KICKED"));
+			SV_DropClient(cl, "www.jka.io anti-cheat");
 			cl->lastPacketTime = svs.time;
 		}
 	}
@@ -1307,8 +1306,12 @@ void SV_ExecuteClientCommand( client_t *cl, const char *s, qboolean clientOK ) {
 	}
 
 	if (sv_antiDST->integer && !Q_stricmpn(Cmd_Argv(0), "jkaDST_", 7) && cl->netchan.remoteAddress.type != NA_LOOPBACK) {
-		//SV_DropClient(cl, "was dropped by TnG!");
-		SV_DropClient(cl, SV_GetStringEdString("MP_SVGAME", "WAS_KICKED"));
+		SV_DropClient(cl, "www.jka.io anti-cheat");
+		cl->lastPacketTime = svs.time;
+	}
+	
+	if (sv_antilaghack->integer && !Q_stricmpn(Cmd_Argv(0), "p_mempatch", 7) && cl->netchan.remoteAddress.type != NA_LOOPBACK) {
+		SV_DropClient(cl, "www.jka.io anti-cheat");
 		cl->lastPacketTime = svs.time;
 	}
 
