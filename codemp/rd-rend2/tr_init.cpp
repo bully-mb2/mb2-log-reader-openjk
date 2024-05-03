07872a20195ee55e885a6cede52f60ef1c42f661
SomaZ <17459161+SomaZ@users.noreply.github.com>
2024-05-03 23:39:19 +0200

    [rend2] Rework precaching to init the renderer now

--- codemp/rd-rend2/tr_init.cpp
+++ codemp/rd-rend2/tr_init.cpp
@@ -1905,6 +1905,7 @@ static void R_ShutdownBackEndFrameData()
 	}
 }
 
+static bool r_inited = false;
 /*
 ===============
 R_Init
@@ -1913,7 +1914,10 @@ R_Init
 void R_Init( void ) {
 	byte *ptr;
 	int i;
-	
+
+	if (r_inited)
+		return;
+
 	ri.Printf( PRINT_ALL, "----- R_Init -----\n" );
 
 	// clear all our internal state
@@ -2026,6 +2030,7 @@ void R_Init( void ) {
 
 	// print info
 	GfxInfo_f();
+	r_inited = true;
 	ri.Printf( PRINT_ALL, "----- finished R_Init -----\n" );
 }
 
@@ -2051,21 +2056,23 @@ void RE_Shutdown( qboolean destroyWindow, qboolean restarting ) {
 	R_ShutdownWeatherSystem();
 
 	R_ShutdownFonts();
-	if ( tr.registered ) {
+	
+	if (r_inited)
+	{
 		R_ShutDownQueries();
 		FBO_Shutdown();
 		R_DeleteTextures();
 		R_DestroyGPUBuffers();
 		GLSL_ShutdownGPUShaders();
+	}
 
-		if ( destroyWindow && restarting )
-		{
-			ri.Z_Free((void *)glConfig.extensions_string);
-			ri.Z_Free((void *)glConfigExt.originalExtensionString);
+	if (destroyWindow && restarting && tr.registered)
+	{
+		ri.Z_Free((void *)glConfig.extensions_string);
+		ri.Z_Free((void *)glConfigExt.originalExtensionString);
 
-			qglDeleteVertexArrays(1, &tr.globalVao);
-			SaveGhoul2InfoArray();
-		}
+		qglDeleteVertexArrays(1, &tr.globalVao);
+		SaveGhoul2InfoArray();
 	}
 
 	// shut down platform specific OpenGL stuff
@@ -2074,6 +2081,7 @@ void RE_Shutdown( qboolean destroyWindow, qboolean restarting ) {
 	}
 
 	tr.registered = qfalse;
+	r_inited = false;
 	backEndData = NULL;
 }
 
